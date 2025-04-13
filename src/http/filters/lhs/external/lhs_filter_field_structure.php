cb1314eca134db81052c6087d708596e1250c008
Your Name <lucas.barreto@leolearning.com>
2025-04-13 14:29:03 -0300

    Fixing external_description type for IN operator

--- src/http/filters/lhs/external/lhs_filter_field_structure.php
+++ src/http/filters/lhs/external/lhs_filter_field_structure.php
@@ -2,6 +2,7 @@
 
 use \core_external\external_single_structure;
 use \core_external\external_value;
+use \moodle_dev_utils\http\filters\lhs\conditions\in_sql_condition;
 
 class lhs_filter_field_structure extends external_single_structure {
 
@@ -14,7 +15,15 @@ class lhs_filter_field_structure extends external_single_structure {
     public function __construct(array $operators = [], $desc = '', $type = PARAM_RAW) {
         $keys = [];
         foreach ($operators as $operator) {
-            $keys[$operator] = new external_value($type, '', VALUE_OPTIONAL);
+            switch ($operator) {
+                case in_sql_condition::get_alias():
+                    $keys[$operator] = new external_value(PARAM_RAW, '', VALUE_OPTIONAL);
+                    break;
+                
+                default:
+                    $keys[$operator] = new external_value($type, '', VALUE_OPTIONAL);
+                    break;
+            }
         }
         
         parent::__construct($keys, $desc, VALUE_OPTIONAL, null);
